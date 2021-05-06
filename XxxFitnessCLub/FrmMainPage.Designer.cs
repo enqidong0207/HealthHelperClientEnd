921d74dd1d879f520bcf727ff5729e103b932574
enqidong0207 <enchi.dong@gmail.com>
2021-05-06 10:10:30 +0800

    加入專案檔案。

--- XxxFitnessCLub/FrmMainPage.Designer.cs
+++ XxxFitnessCLub/FrmMainPage.Designer.cs
@@ -29,22 +29,35 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnComment = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.button15 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
             this.btnMealLogPage = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.btnWorkoutLogPage = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.btnHomePage = new System.Windows.Forms.Button();
             this.btnMemberPage = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,24 +69,41 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblUser);
+            this.splitContainer1.Panel1.Controls.Add(this.btnComment);
             this.splitContainer1.Panel1.Controls.Add(this.button16);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.button15);
             this.splitContainer1.Panel1.Controls.Add(this.button14);
             this.splitContainer1.Panel1.Controls.Add(this.btnMealLogPage);
+            this.splitContainer1.Panel1.Controls.Add(this.button11);
             this.splitContainer1.Panel1.Controls.Add(this.btnWorkoutLogPage);
             this.splitContainer1.Panel1.Controls.Add(this.button7);
             this.splitContainer1.Panel1.Controls.Add(this.button6);
             this.splitContainer1.Panel1.Controls.Add(this.btnHomePage);
             this.splitContainer1.Panel1.Controls.Add(this.btnMemberPage);
+            this.splitContainer1.Panel1.Controls.Add(this.button8);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
             this.splitContainer1.Size = new System.Drawing.Size(1116, 656);
             this.splitContainer1.SplitterDistance = 184;
+            this.splitContainer1.Size = new System.Drawing.Size(1208, 656);
+            this.splitContainer1.SplitterDistance = 199;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // btnComment
+            // 
+            this.btnComment.Location = new System.Drawing.Point(20, 332);
+            this.btnComment.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnComment.Name = "btnComment";
+            this.btnComment.Size = new System.Drawing.Size(131, 32);
+            this.btnComment.TabIndex = 31;
+            this.btnComment.Text = "留言板";
+            this.btnComment.UseVisualStyleBackColor = true;
+            this.btnComment.Click += new System.EventHandler(this.btnComment_Click);
             // 
             // button16
             // 
@@ -94,6 +124,7 @@
             this.label3.Location = new System.Drawing.Point(34, 469);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 23);
+            this.label3.Size = new System.Drawing.Size(144, 28);
             this.label3.TabIndex = 28;
             this.label3.Text = "網頁收到左邊";
             // 
@@ -120,6 +151,7 @@
             this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // btnMealLogPage
+            // button11
             // 
             this.btnMealLogPage.Location = new System.Drawing.Point(20, 156);
             this.btnMealLogPage.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
@@ -129,6 +161,14 @@
             this.btnMealLogPage.Text = "紀錄飲食";
             this.btnMealLogPage.UseVisualStyleBackColor = true;
             this.btnMealLogPage.Click += new System.EventHandler(this.btnMealPage_Click);
+            this.button11.Location = new System.Drawing.Point(20, 156);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(131, 32);
+            this.button11.TabIndex = 25;
+            this.button11.Text = "紀錄飲食";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // btnWorkoutLogPage
             // 
@@ -164,6 +204,7 @@
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // btnHomePage
+            // button8
             // 
             this.btnHomePage.Location = new System.Drawing.Point(20, 10);
             this.btnHomePage.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
@@ -173,6 +214,14 @@
             this.btnHomePage.Text = "回首頁";
             this.btnHomePage.UseVisualStyleBackColor = true;
             this.btnHomePage.Click += new System.EventHandler(this.btnHomePage_Click);
+            this.button8.Location = new System.Drawing.Point(20, 107);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(131, 32);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "會員資料";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.btnMember_Click);
             // 
             // btnMemberPage
             // 
@@ -193,25 +242,154 @@
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             this.splitContainer2.Size = new System.Drawing.Size(927, 656);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label4);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Panel2.Controls.Add(this.button5);
+            this.splitContainer2.Panel2.Controls.Add(this.button4);
+            this.splitContainer2.Panel2.Controls.Add(this.button3);
+            this.splitContainer2.Panel2.Controls.Add(this.button2);
+            this.splitContainer2.Panel2.Controls.Add(this.button1);
+            this.splitContainer2.Size = new System.Drawing.Size(1004, 656);
             this.splitContainer2.SplitterDistance = 95;
             this.splitContainer2.SplitterWidth = 5;
             this.splitContainer2.TabIndex = 0;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(771, 485);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "不管去哪一直在";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(385, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "主替換panel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(182, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time Limited";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button4.Location = new System.Drawing.Point(0, 517);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1000, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "遊戲規則";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 26F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = global::XxxFitnessCLub.Properties.Resources.scale_lose_weight_2_1080x6751;
+            this.button5.Location = new System.Drawing.Point(34, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(222, 190);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "更新體重";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::XxxFitnessCLub.Properties.Resources.螢幕擷取畫面_2021_05_02_204709;
+            this.button3.Location = new System.Drawing.Point(309, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(222, 190);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "運動日記";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::XxxFitnessCLub.Properties.Resources.images;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(595, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 190);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "推薦運動";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 26F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::XxxFitnessCLub.Properties.Resources.MealRed;
+            this.button1.Location = new System.Drawing.Point(34, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 190);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "紀錄飲食";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(20, 25);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(61, 22);
+            this.lblUser.TabIndex = 32;
+            this.lblUser.Text = "用戶: ";
+            
+            // 
             // FrmMainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1116, 656);
+            this.ClientSize = new System.Drawing.Size(1208, 656);
             this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("Consolas", 9F);
             this.Name = "FrmMainPage";
             this.Text = "FrmMainPage";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmMainPage_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -222,15 +400,27 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button14;
         private System.Windows.Forms.Button btnMealLogPage;
+        private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button btnWorkoutLogPage;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button btnMemberPage;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button btnHomePage;
+        private System.Windows.Forms.Button btnComment;
+        private System.Windows.Forms.Label lblUser;
     }
 }
--- XxxFitnessCLub/FrmMainPage.cs
+++ XxxFitnessCLub/FrmMainPage.cs
@@ -1,4 +1,6 @@
 ﻿using HHFirstDraft;
+using HHFirstDraft.BLL;
+using HHFirstDraft.DAL.DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,9 +22,14 @@ namespace XxxFitnessCLub
         }
 
         private void FrmMainPage_Load(object sender, EventArgs e)
+        private void btnMember_Click(object sender, EventArgs e)
         {
             FrmHome frm = new FrmHome();
+            //this.Controls.Clear();
+            FrmAddMember frm = new FrmAddMember();
             frm.TopLevel = false;
+            frm.isUpdate = true;
+            frm.detail = memberDetail;
             frm.AutoScroll = true;
             this.splitContainer2.Panel2.Controls.Add(frm);
             frm.FormBorderStyle = FormBorderStyle.None;
@@ -32,7 +39,11 @@ namespace XxxFitnessCLub
 
         //回首頁
         private void btnHomePage_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e)
         {
+            FrmGiftCard f = new FrmGiftCard();
+            f.ShowDialog();
+        }
 
             FrmHome frm = new FrmHome();
             frm.TopLevel = false;
@@ -41,12 +52,62 @@ namespace XxxFitnessCLub
             frm.FormBorderStyle = FormBorderStyle.None;
             frm.Dock = DockStyle.Fill;
             frm.Show();
+        private void button15_Click(object sender, EventArgs e)
+        {
+            
+             FrmMealWorkoutHistory f = new FrmMealWorkoutHistory();
+            f.Show();
         }
 
         //會員資料
         private void btnMemberPage_Click(object sender, EventArgs e)
+        private void button14_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FrmStartAProgram f = new FrmStartAProgram();
+            f.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FrmRules f = new FrmRules();
+            f.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmMealLog f = new FrmMealLog();
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmWorkoutSuggestions f = new FrmWorkoutSuggestions();
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FrmWorkoutLog f = new FrmWorkoutLog();
+            f.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
             FrmMemberProfile f = new FrmMemberProfile();
             f.TopLevel = false;
             f.AutoScroll = true;
@@ -58,6 +119,7 @@ namespace XxxFitnessCLub
 
         //紀錄飲食
         private void btnMealPage_Click(object sender, EventArgs e)
+        private void button10_Click_1(object sender, EventArgs e)
         {
             FrmMealLog frm = new FrmMealLog();
             frm.TopLevel = false;
@@ -69,6 +131,17 @@ namespace XxxFitnessCLub
         }
 
         //運動Log
+        private void button11_Click(object sender, EventArgs e)
+        {
+            FrmMealLog f = new FrmMealLog();
+            f.Show();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void btnWorkoutLogPage_Click(object sender, EventArgs e)
         {
             FrmWorkoutLog frm = new FrmWorkoutLog();
@@ -89,30 +162,42 @@ namespace XxxFitnessCLub
 
         //吃過動過
         private void button15_Click(object sender, EventArgs e)
+        private void btnComment_Click(object sender, EventArgs e)
         {
 
             FrmMealWorkoutHistory f = new FrmMealWorkoutHistory();
             f.Show();
         }
-
+            FrmComment frm = new FrmComment();
+            frm.TopLevel = false;
+            frm.AutoScroll = true;
+            
         //禮物卡
         private void button6_Click(object sender, EventArgs e)
         {
             FrmGiftCard f = new FrmGiftCard();
             f.ShowDialog();
+            this.splitContainer2.Panel2.Controls.Add(frm);
+            frm.FormBorderStyle = FormBorderStyle.None;
+            frm.Show();
         }
         
         //我的表現
         private void button14_Click(object sender, EventArgs e)
+        MemberDetailDTO memberDetail = new MemberDetailDTO();
+        MemberBLL memberBLL = new MemberBLL();
+        private void FrmMainPage_Load(object sender, EventArgs e)
         {
             // todo 我的表現
         }
-
+            
         //開始計畫
         private void button7_Click(object sender, EventArgs e)
         {
             FrmStartAProgram f = new FrmStartAProgram();
             f.Show();
+            memberDetail = memberBLL.GetMember(UserStatic.UserID);
+            lblUser.Text = "Welcome back, " + UserStatic.UserName + "!";
         }
     }
 }
