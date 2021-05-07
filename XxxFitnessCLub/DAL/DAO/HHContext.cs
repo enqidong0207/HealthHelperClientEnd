d856fc409257cf17d9511baa2ee13fc8c3d1d88e
enqidong0207 <enchi.dong@gmail.com>
2021-05-07 11:28:01 +0800

    fix cherry pick

---
+++ XxxFitnessCLub/DAL/DAO/HHContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XxxFitnessCLub.DAL;
+
+namespace HHFirstDraft.DAL.DAO
+{
+    public class HHContext
+    {
+        public HealthManagement0505Entities db = new HealthManagement0505Entities(); 
+    }
+}
