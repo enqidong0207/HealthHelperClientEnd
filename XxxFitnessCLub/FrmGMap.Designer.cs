4fe8f15fc6bf09e46c7d1a8980045e853694b5fd
enqidong0207 <enchi.dong@gmail.com>
2021-05-10 10:00:31 +0800

    gmap installed

---
+++ XxxFitnessCLub/FrmGMap.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace XxxFitnessCLub
+{
+    partial class FrmGMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(689, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "搜尋";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(582, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(219, 409);
+            this.listBox1.TabIndex = 3;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(88, 34);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(407, 341);
+            this.gMapControl1.TabIndex = 4;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // FrmGMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmGMap";
+            this.Text = "FrmGMap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
---
+++ XxxFitnessCLub/FrmGMap.cs
@@ -0,0 +1,74 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XxxFitnessCLub
+{
+    public partial class FrmGMap : Form
+    {
+        public List<PointLatLng> searchResult;
+        public GeocodingProvider gp;
+        public YahooMapProvider gmp;
+
+        public FrmGMap()
+        {
+            InitializeComponent();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+
+            // Do not suppress prompt, and wait 1000 milliseconds to start.
+            GeoCoordinate coord = new GeoCoordinate();
+            while (coord.IsUnknown)
+            {
+                watcher.TryStart(true, TimeSpan.FromMilliseconds(5000));
+                coord = watcher.Position.Location;
+            }
+
+            if (coord.IsUnknown != true)
+            {
+                MessageBox.Show($"Lat: {coord.Latitude}, Long: {coord.Longitude}");
+            }
+            else
+            {
+                MessageBox.Show("Unknown latitude and longitude.");
+            }
+
+            //初始化地圖為google map 並設定初始中心位置為china
+            //gMap.MapProvider = GMap.NET.MapProviders.GoogleChinaMapProvider.Instance;
+
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            this.gMapControl1.MapProvider = GMapProviders.YahooMap;
+            //gmp = this.gMap.MapProvider as YahooMapProvider;
+            //gmp.ApiKey = "AIzaSyAE3Hi6N9QONHypztdZAvYkdTIOXdnzNE4";
+
+            //gMap.Position = new PointLatLng(45.74740199642105, 126.69570922851562);
+            //gMap.SetPositionByKeywords("china,harbin");//設定初始中心為china harbin
+
+            this.gMapControl1.Position = new PointLatLng(coord.Latitude, coord.Longitude);
+            gMapControl1.Position = new PointLatLng(25.03746, 121.564558);
+
+            //gp = this.gMap.MapProvider as GeocodingProvider;
+            //GMapProvider.Language = LanguageType.ChineseTraditional;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GeoCoderStatusCode statusCode = gmp.GetPoints("park", out searchResult);
+        }
+    }
+}
