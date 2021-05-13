d0a78faf519328e85b80eb46688a070fb84502c1
enqidong0207 <enchi.dong@gmail.com>
2021-05-13 15:05:27 +0800

    merge to master 2nd try

---
+++ XxxFitnessCLub/BLL/WorkoutLogBLL.cs
@@ -0,0 +1,101 @@
+﻿using HHFirstDraft.DAL;
+using HHFirstDraft.DAL.DAO;
+using HHFirstDraft.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XxxFitnessCLub;
+using XxxFitnessCLub.DAL;
+
+namespace HHFirstDraft.BLL
+{
+    //恩旗
+    class WorkoutLogBLL
+    {
+        WorkoutLogDAO dao = new WorkoutLogDAO();
+        const int TAKEDAYS = 14;
+
+        internal List<WorkoutLogDTO> GetWorkoutLogs()
+        {
+            List<WorkoutLogDTO> list = new List<WorkoutLogDTO>();
+            foreach (var wl in dao.GetWorkoutLogs())
+            {
+                list.Add(new WorkoutLogDTO(wl));
+            }
+            return list;
+        }
+
+        internal bool Add(WorkoutLog entity)
+        {
+            return dao.Add(entity);
+        }
+
+        internal List<DailyWorkoutCal> GetWorkoutCalByDate(DateTime? startDate, DateTime? endDate)
+        {
+            DateTime sd = startDate ?? DateTime.MinValue;
+            DateTime ed = endDate ?? DateTime.MaxValue;
+            
+            var q = (from wl in GetWorkoutLogs()
+                     .Where(wl => wl.MemberID == UserStatic.UserID)
+                     .Where(wl => wl.EditTime.Date >= sd.Date && wl.EditTime.Date <= ed.Date)
+                     group wl by wl.EditTime.Date into g
+                     select new DailyWorkoutCal
+                     {
+                         EditDate = g.Key,
+                         TotalCal = $"{g.Sum(wl => wl.WorkoutTotalCal):0.00}",
+                         TotalHours = $"{g.Sum(wl => wl.WorkoutHours):0.00}"
+                     }).OrderBy(g => g.EditDate);
+
+
+            if (q.ToList().Count > TAKEDAYS)
+            {
+                int skipDays = q.ToList().Count - TAKEDAYS;
+                return q.Skip(skipDays).ToList();
+            }
+            else
+            {
+                return q.Take(14).ToList();
+            }
+
+        }
+        
+        internal WorkoutLogDTO GetWorkoutLogByID(int ID)
+        {
+            return GetWorkoutLogs().SingleOrDefault(r => r.ID == ID);
+        }
+
+        internal bool Edit(WorkoutLog entity)
+        {
+            return dao.Edit(entity);
+        }
+
+        internal bool Delete(WorkoutLog entity)
+        {
+            return dao.Delete(entity);
+        }
+
+        internal List<WorkoutLogDTO> GetWorkoutLogsByKeyword(string keyword)
+        {
+            if (keyword == null)
+            {
+                keyword = "";
+            }
+            
+            return GetWorkoutLogs().Where(wl => wl.MemberID == UserStatic.UserID)
+                    .Where(wl => wl.WorkoutName.Contains(keyword)
+                    || wl.WorkoutCategoryName.Contains(keyword)
+                    || wl.ActivityLevelName.Contains(keyword)).ToList();
+            
+        }
+    }
+
+    //恩旗
+    class DailyWorkoutCal
+    {
+        public DateTime EditDate { get; set; }
+        public string TotalCal { get; set; }
+        public string TotalHours { get; set; }
+    }
+}
---
+++ XxxFitnessCLub/DAL/DAO/WorkoutLogDAO.cs
@@ -0,0 +1,70 @@
+﻿using HHFirstDraft.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XxxFitnessCLub.DAL;
+
+namespace HHFirstDraft.DAL.DAO
+{
+    //恩旗
+    class WorkoutLogDAO : HHContext
+    {
+        internal List<WorkoutLog> GetWorkoutLogs()
+        {
+            return db.WorkoutLogs.ToList();
+        }
+
+        internal bool Add(WorkoutLog entity)
+        {
+            try
+            {
+                db.WorkoutLogs.Add(entity);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            
+        }
+
+        internal bool Edit(WorkoutLog entity)
+        {
+            WorkoutLog oldEntity = db.WorkoutLogs.Where(wl => wl.ID == entity.ID).SingleOrDefault();
+            try
+            {
+                oldEntity.WorkoutID = entity.WorkoutID;
+                oldEntity.EditTime = entity.EditTime;
+                oldEntity.WorkoutHours = entity.WorkoutHours;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        internal bool Delete(WorkoutLog entity)
+        {
+            WorkoutLog oldEntity = db.WorkoutLogs.Where(wl => wl.ID == entity.ID).SingleOrDefault();
+            try
+            {
+                db.WorkoutLogs.Remove(oldEntity);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
